fb6431f3ac9bfeb68fafd863bc1fee0b3607fc35
shindongeok <ehrflqakstp@gmail.com>
2024-01-11 23:51:00 +0900

    한 화면에 회원수정 탈퇴 화면 띄우기는 됨.. 클래스 수정..

--- script/elimination.js
+++ script/elimination.js
@@ -1,4 +1,16 @@
-const modal = document.querySelector(".modal");
+const modalTest = document.querySelector(".modal");
+const btnOpenModalTest = document.querySelector(".btn_open_modal");
+const closeTest = document.querySelector(".close");
+
+btnOpenModalTest.addEventListener("click", () => {
+  modalTest.style.display = "flex";
+});
+
+closeTest.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+const modal = document.querySelector(".modal__elimination");
 const btnOpenModal = document.querySelector(".btn_open_elimination");
 const close = document.querySelector(".elimination__close");
 
