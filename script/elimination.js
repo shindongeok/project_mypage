8e610b9cf082d6a91c44b82d56614a15738d1a0c
shindongeok <ehrflqakstp@gmail.com>
2024-01-12 03:11:28 +0900

    70% 완료... 모달창 꾸미기미완료

--- script/elimination.js
+++ script/elimination.js
@@ -1,3 +1,4 @@
+// 회원수정 모달창 띄우고 닫기
 const modalTest = document.querySelector(".modal");
 const btnOpenModalTest = document.querySelector(".btn_open_modal");
 const closeTest = document.querySelector(".close");
@@ -10,6 +11,7 @@ closeTest.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+// 회원탈퇴 모달창 띄우고 닫기
 const modal = document.querySelector(".modal__elimination");
 const btnOpenModal = document.querySelector(".btn_open_elimination");
 const close = document.querySelector(".elimination__close");
