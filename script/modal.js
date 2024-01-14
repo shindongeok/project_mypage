d1e180e9bc11f0341c021ebf3dc0e6aa4c749fbd
shindongeok <ehrflqakstp@gmail.com>
2024-01-14 22:32:53 +0900

    Revert "회원탈퇴 모달창 두개.."
    This reverts commit e8a2ba8fc1eaace4047e667a72bd73024a4ec862.

--- script/modal.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".btn_open_elimination").click(function () {
-    $(".modify__modal_bg").fadeIn();
-  });
-
-  $(".close_01").click(function () {
-    $(".modify__modal_bg").fadeOut();
-  });
-});
