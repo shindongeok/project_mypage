d6d242adc7f47bc9dae0cc416fc113c0885c6c11
shindongeok <ehrflqakstp@gmail.com>
2024-01-14 23:11:09 +0900

    회원탈퇴 수정부분

---
+++ script/modal.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".btn_open_elimination").click(function () {
+    $(".modify__modal_bg").fadeIn();
+  });
+
+  $(".close_01").click(function () {
+    $(".modify__modal_bg").fadeOut();
+  });
+});
